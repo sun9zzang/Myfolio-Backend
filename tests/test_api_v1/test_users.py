d411fa48e43a31419c3743d6af52b8704a19cb62
sun9zzang <sun9nyy@gmail.com>
2022-11-16 15:48:54 +0900

    WIP: unit test - Users API

--- tests/test_api_v1/test_users.py
+++ tests/test_api_v1/test_users.py
@@ -2,6 +2,34 @@ import pytest
 from fastapi import FastAPI, status
 from fastapi.testclient import TestClient
 
+from app.core.schemas.users import User, UserInDB
+from app.core.schemas.errors import ErrorList
+from app.core.errors.errors import ManagedErrors
+
+
+class _TestUserData:
+    wrong_email = []
+    wrong_username = []
+    wrong_password = []
+
+    invalid_token = []
+    invalid_token_prefix = []
+
+
+@pytest.fixture(params=[])
+def invalid_authorization_header(request):
+    return request.param
+
+
+@pytest.fixture(params=_TestUserData.invalid_token)
+def invalid_token(request):
+    return request.param
+
+
+@pytest.fixture(params=_TestUserData.invalid_token_prefix)
+def invalid_token_prefix(request):
+    return request.param
+
 
 # POST /v1/users
 # 신규 유저를 생성합니다.
@@ -18,7 +46,10 @@ class TestV1UsersPOST:
             ("email", 'this is"not\\allowed@example.com'),
             ("email", 'this\\ still"notallowed@example.com'),
             ("email", "weird@examplecom"),  # no dots on domain-part
-            ("email", "weir.do@examplecom"),  # dots on local-part, no dots on domain-part
+            (
+                "email",
+                "weir.do@examplecom",
+            ),  # dots on local-part, no dots on domain-part
             ("username", "악"),  # too short
             # ("username", "qq"),  # too short
             ("username", "으악악악악엥뜨악힝"),  # too long
@@ -30,7 +61,10 @@ class TestV1UsersPOST:
             ("password", "onlyalphapwd"),  # only alphas
             ("password", "135871394137"),  # only digits
             ("password", "short"),  # too short
-            ("password", "tooooo000000000000000000looooo0000000000000000000ngbabyyyyyyyyyyyyyyyyyyyyyyyy"),  # too long
+            (
+                "password",
+                "tooooo000000000000000000looooo0000000000000000000ngbabyyyyyyyyyyyyyyyyyyyyyyyy",
+            ),  # too long
             ("password", "hgowrgwf@@@"),  # no digits
             ("password", "$$$$3213743"),  # no alphas
             # ("password", "password1234"),  # too weak
@@ -46,6 +80,123 @@ class TestV1UsersPOST:
     ):
         user_dict.update({wrong_field: wrong_value})
         print(f"attempting to create user with invalid field - user_dict: {user_dict}")
-        response = client.post(app.url_path_for("users:create-user"))
+        response = client.post(
+            app.url_path_for("users:create-user"),
+            json=user_dict,
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST, ""
+        assert (
+            response.json()["errors"][0]["code"] == "invalid_" + wrong_field
+        ), ""  # todo
+
+    # 중복된 필드로 유저 생성 요청 불가
+    def test_cannot_create_user_with_duplicated_field(
+        self,
+        app: FastAPI,
+        user: UserInDB,
+        client: TestClient,
+    ):
+        raise NotImplementedError
+
+    # 유효한 필드로 유저 생성 요청 가능
+    def test_can_create_user(
+        self,
+        app: FastAPI,
+        user_dict: dict,
+        client: TestClient,
+    ):
+        print(f"attempting to create user - user_dict: {user_dict}")
+        response = client.post(
+            app.url_path_for("users:create-user"),
+            json=user_dict,
+        )
+
+        assert response.status_code == status.HTTP_201_CREATED, ""
+        assert User(**response.json()).dict() == user_dict, ""
 
 
+class TestV1UsersRetrieveUserGET:
+
+    # 잘못된 user_id로 유저 정보 요청 불가
+    @pytest.mark.parametrize(
+        "invalid_user_id",
+        [
+            "abcdef",
+            "가나다",
+            "?",
+        ],
+    )
+    def test_cannot_retrieve_user_with_invalid_syntax(
+        self,
+        app: FastAPI,
+        user: UserInDB,
+        client: TestClient,
+        invalid_user_id: str,
+    ):
+        print(
+            f"attempting to retrieve user with invalid syntax - invalid_user_id: {invalid_user_id}"
+        )
+        response = client.get(
+            app.url_path_for("users:retrieve-user", user_id=invalid_user_id)
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST, ""
+        assert (
+            response.json() == ErrorList().append(ManagedErrors.bad_request).dict()
+        ), ""
+
+    # 존재하지 않는 user_id로 유저 정보 요청 불가
+    def test_cannot_retrieve_user_with_not_existing_user_id(
+        self,
+        app: FastAPI,
+        client: TestClient,
+    ):
+        raise NotImplementedError
+
+    # 존재하는 유저 정보 요청 가능
+    def test_can_retrieve_user(
+        self,
+        app: FastAPI,
+        user: UserInDB,
+        client: TestClient,
+    ):
+        print(f"attempting to retrieve user - user_id: {user.user_id}")
+        response = client.get(
+            app.url_path_for("users:retrieve-user", user_id=user.user_id)
+        )
+
+        assert response.status_code == status.HTTP_200_OK, ""
+        assert response.json() == user.dict(
+            exclude={"salt": True, "hashed_password": True}
+        ), ""
+
+
+class TestV1UsersUpdateUserPATCH:
+
+    # Authorization header 없이 유저 업데이트 요청 불가
+    def test_cannot_update_user_without_authentication(
+        self,
+        app: FastAPI,
+        user: UserInDB,
+        client: TestClient,
+    ):
+        print(f"attempting to update user without authentication...")
+
+        raise NotImplementedError
+
+    # 유효하지 않은 인증 정보로 유저 업데이트 요청 불가
+    def test_cannot_update_user_with_invalid_authentication(
+        self,
+        app: FastAPI,
+        user: UserInDB,
+        client: TestClient,
+        authorization_header_key: str,
+    ):
+        header = {authorization_header_key: ...}
+        client.headers.update(header)
+        print(
+            f"attempting to update user with invalid authentication - header: {header}"
+        )
+
+        raise NotImplementedError
